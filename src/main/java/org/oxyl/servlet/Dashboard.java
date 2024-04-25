8981db872d37da0bf634bfb75be4cdcae269b8d9
Seboubi <slatronche@oxyl.fr>
2024-04-25 17:17:15 +0200

    commit de fin de journée, dashboard en construction, vérifier pk la boucle for fonctionne pas et ne fais pas crash

--- src/main/java/org/oxyl/servlet/Dashboard.java
+++ src/main/java/org/oxyl/servlet/Dashboard.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.oxyl.model.Stagiaire;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,13 +19,13 @@ public class Dashboard extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-//        Optional<List<Stagiaire>> optStagiaires = getAllStagiaires();
-//        List<Stagiaire> stagiaires = null;
-//        if (optStagiaires.isPresent()) {
-//            stagiaires = optStagiaires.get();
-//        }
-//
-//        request.setAttribute("stagiaires", stagiaires);
+        Optional<List<Stagiaire>> optStagiaires = getAllStagiaires();
+        List<Stagiaire> stagiaires = null;
+        if (optStagiaires.isPresent()) {
+            stagiaires = optStagiaires.get();
+        }
+
+        request.setAttribute("stagiaires", stagiaires);
 
         request.getRequestDispatcher("WEB-INF/dashboard.jsp").forward(request, response);
     }
--- src/main/resources/2-PRIVILEGES.sql
+++
@@ -1,8 +0,0 @@
-  #-----------------------------------
-  #USER RIGHTS MANAGEMENT
-  #-----------------------------------
-  CREATE USER 'adminnewro'@'127.0.0.1' IDENTIFIED BY 'Qw€rty1234';
-
-  GRANT ALL PRIVILEGES ON `newro-factory-db`.* TO 'adminnewro'@'127.0.0.1' WITH GRANT OPTION;
-
-  FLUSH PRIVILEGES;
